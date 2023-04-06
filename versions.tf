23f984180fed60814e93606f944cd485386872e3
nileshgadgi <nilesh.gadgi@clouddrove.com>
2023-04-06 13:07:48 +0530

    [Hurodata-50] fix - azurerm provider required version changed to >=3.0.0 in

--- versions.tf
+++ versions.tf
@@ -1,12 +1,12 @@
 terraform {
-  required_version = ">= 1.0.0"
+  required_version = ">= 1.3.7"
 }
 
 terraform {
   required_providers {
     azurerm = {
       source  = "hashicorp/azurerm"
-      version = ">=2.90.0"
+      version = ">=3.0.0"
     }
   }
 }
