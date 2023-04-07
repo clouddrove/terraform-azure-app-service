81c0b432078aa3f9e9366487487329f2c053df8a
nileshgadgi <nilesh.gadgi@clouddrove.com>
2023-04-07 20:02:55 +0530

    [Hurodata-51] fix - ran terraform fmt command

--- main.tf
+++ main.tf
@@ -33,23 +33,23 @@ locals {
   }
 
   ip_address = [for ip_address in var.ips_allowed : {
-  name                      = "ip_restriction_cidr_${join("", [1, index(var.ips_allowed, ip_address)])}"
-  ip_address                = ip_address
-  virtual_network_subnet_id = null
-  service_tag               = null
-  subnet_id                 = null
-  priority                  = join("", [1, index(var.ips_allowed, ip_address)])
-  action                    = "Allow"
+    name                      = "ip_restriction_cidr_${join("", [1, index(var.ips_allowed, ip_address)])}"
+    ip_address                = ip_address
+    virtual_network_subnet_id = null
+    service_tag               = null
+    subnet_id                 = null
+    priority                  = join("", [1, index(var.ips_allowed, ip_address)])
+    action                    = "Allow"
   }]
 
   subnets = [for subnet in var.subnet_ids_allowed : {
-  name                      = "ip_restriction_subnet_${join("", [1, index(var.subnet_ids_allowed, subnet)])}"
-  ip_address                = null
-  virtual_network_subnet_id = subnet
-  service_tag               = null
-  subnet_id                 = subnet
-  priority                  = join("", [1, index(var.subnet_ids_allowed, subnet)])
-  action                    = "Allow"
+    name                      = "ip_restriction_subnet_${join("", [1, index(var.subnet_ids_allowed, subnet)])}"
+    ip_address                = null
+    virtual_network_subnet_id = subnet
+    service_tag               = null
+    subnet_id                 = subnet
+    priority                  = join("", [1, index(var.subnet_ids_allowed, subnet)])
+    action                    = "Allow"
   }]
 }
 
